5b8738b5daa70c53c468a4106760abfa7d8f8c98
ivanovich1071 <163851033+ivanovich1071@users.noreply.github.com>
2024-08-19 20:06:25 +0300

    1908 new botrun

--- botrun.py
+++ botrun.py
@@ -3,7 +3,7 @@ import asyncio
 from aiogram import Bot, Dispatcher, types, Router
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, CallbackQuery
-from keyboards import create_greeting_keyboard, create_links_keyboard, create_dynamic_keyboard, create_options_keyboard
+from keyboards import create_greeting_keyboard, create_links_keyboard, create_dynamic_keyboard, create_options_keyboard, create_menu_keyboard
 import config
 
 # Логирование для отслеживания работы бота
@@ -19,12 +19,14 @@ dp.include_router(router)
 async def send_welcome(message: Message):
     await message.answer("Выберите опцию:", reply_markup=create_greeting_keyboard())
 
-@router.message(lambda message: message.text in ["Привет", "Пока"])
+@router.message(lambda message: message.text in ["Привет", "Пока", "Меню"])
 async def handle_greeting(message: Message):
     if message.text == "Привет":
         await message.answer(f"Привет, {message.from_user.first_name}!")
     elif message.text == "Пока":
         await message.answer(f"До свидания, {message.from_user.first_name}!")
+    elif message.text == "Меню":
+        await message.answer("Выберите ссылку:", reply_markup=create_links_keyboard())
 
 @router.message(Command(commands=['links']))
 async def send_links(message: Message):
@@ -36,12 +38,22 @@ async def send_dynamic(message: Message):
 
 @router.callback_query(lambda call: call.data == "show_more")
 async def show_more_options(callback_query: CallbackQuery):
-    await callback_query.message.edit_reply_markup(reply_markup=create_options_keyboard())
+    await callback_query.message.edit_text("Выберите опцию:", reply_markup=create_options_keyboard())
 
-@router.callback_query(lambda call: call.data in ["option_1", "option_2"])
+@router.callback_query(lambda call: call.data in ["option_1", "option_2", "option_3", "back_to_menu"])
 async def handle_option_selection(callback_query: CallbackQuery):
-    selection = "Опция 1" if callback_query.data == "option_1" else "Опция 2"
-    await callback_query.message.answer(f"Вы выбрали: {selection}")
+    if callback_query.data == "option_1":
+        await callback_query.message.edit_text("Вот список новостей", reply_markup=create_menu_keyboard())
+    elif callback_query.data == "option_2":
+        await callback_query.message.edit_text("Вот плей-лист музыки", reply_markup=create_menu_keyboard())
+    elif callback_query.data == "option_3":
+        await callback_query.message.edit_text("Вот список роликов", reply_markup=create_menu_keyboard())
+    elif callback_query.data == "back_to_menu":
+        await callback_query.message.answer("Выберите опцию:", reply_markup=create_greeting_keyboard())
+
+@router.message(lambda message: True)
+async def unknown_message(message: Message):
+    await message.answer("Извините, я не понимаю эту команду. Пожалуйста, выберите опцию из меню.", reply_markup=create_greeting_keyboard())
 
 async def main():
     # Запуск бота
@@ -50,4 +62,3 @@ async def main():
 
 if __name__ == '__main__':
     asyncio.run(main())
-
--- keyboards.py
+++ keyboards.py
@@ -1,21 +1,21 @@
-
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
-# Создание Reply клавиатуры с кнопками "Привет" и "Пока"
+# Создание Reply клавиатуры с кнопками "Привет", "Пока" и "Меню"
 def create_greeting_keyboard():
     builder = ReplyKeyboardBuilder()
     builder.add(KeyboardButton(text="Привет"))
     builder.add(KeyboardButton(text="Пока"))
+    builder.add(KeyboardButton(text="Меню"))
     builder.adjust(2)  # Равномерное распределение кнопок
     return builder.as_markup(resize_keyboard=True)
 
 # Создание Inline клавиатуры с кнопками для новостей, музыки и видео
 def create_links_keyboard():
     builder = InlineKeyboardBuilder()
-    builder.add(InlineKeyboardButton(text="Новости", url="https://habr.com/ru/companies/cloud4y/articles/"))
-    builder.add(InlineKeyboardButton(text="Музыка", url="https://pixabay.com/ru/music/battle-of-the-dragons-8037/"))
-    builder.add(InlineKeyboardButton(text="Видео", url="https://videos.pexels.com/video-files/1409899/1409899-uhd_2560_1440_25fps.mp4 "))
+    builder.add(InlineKeyboardButton(text="Новости", callback_data="option_1"))
+    builder.add(InlineKeyboardButton(text="Музыка", callback_data="option_2"))
+    builder.add(InlineKeyboardButton(text="Видео", callback_data="option_3"))
     builder.adjust(1)  # Все кнопки в одном ряду
     return builder.as_markup()
 
@@ -32,3 +32,10 @@ def create_options_keyboard():
     builder.add(InlineKeyboardButton(text="вернутся", callback_data="option_2"))
     builder.adjust(1)  # Кнопки в одном ряду
     return builder.as_markup()
+
+# Клавиатура для возврата в основное меню
+def create_menu_keyboard():
+    builder = InlineKeyboardBuilder()
+    builder.add(InlineKeyboardButton(text="Назад в меню", callback_data="back_to_menu"))
+    return builder.as_markup()
+
